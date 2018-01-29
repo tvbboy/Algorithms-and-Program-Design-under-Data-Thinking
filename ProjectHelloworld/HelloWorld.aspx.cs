583db87c55886cf2c99699cd4a0e1b22469f02d2
PuPeng <puxiaoming@163.com>
2018-01-29 13:48:25 +0800

    这是我第一次提交

---
+++ ProjectHelloworld/HelloWorld.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProjectHelloworld
+{
+    public partial class HelloWorld : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.Title = "这是我的第一个动态网页";
+            Response.Write("Hello World!");
+        }
+    }
+}
