0b61cb25b92433a1e9278e4a5cfaf286d34a1998
jiangsc17 <jiangsc17@qq.com>
2020-03-15 19:54:07 +0800

    2020/3/15 19:54 Shangchen Jiang

--- ProjectHelloworld/homeworkGetTriangularArea.aspx.cs
+++ ProjectHelloworld/homeworkGetTriangularArea.aspx.cs
@@ -5,9 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace ProjectHelloWorld
+namespace ProjectHelloworld
 {
-    public partial class homeworkTriangularArea : System.Web.UI.Page
+    public partial class homeworkGetTriangularArea : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
